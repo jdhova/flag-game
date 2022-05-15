e98d16d6b8785319f820c6c65bdb0b97339b0ea4
Jude Okagu <juud0010@gmail.com>
2022-05-14 22:58:43 -0400

    fixing

--- js/script.js
+++ js/script.js
@@ -30,15 +30,16 @@ const flags = [
 
 const countries =  [
     "Argentina", "Aruba,","Antactica" ,
-    "Brown", "Brazil", "Bell", "Brown",
+    "Brown", "Brazil", "Belgium", 
     "Call", "Car", "Canada", "Allow",
 
 ]
+
+const answer1 = 'Argentina';
+const answer2 = 'Brazil';
 //    {option1:"Argentina",option2: "Aruba", option3:"Antactica"} 
    
-   
 
-  
  
      const nextFlag = () =>{
          
@@ -49,20 +50,119 @@ const countries =  [
         img.src = flags[index];
     };
 
-    next.addEventListener('click',nextFlag);
 
-
-    // const nextCountry = () =>{
+    const nextCountry = () =>{
          
-    //     index++;
-    //     if(countryIndex >= countries.length){
-    //         countryIndex = 0;
-    //     }
-    //     console.log('hehehe',countries[0]);
-    //       option1.innerHTML = countries[0];
-    //      option2.innerHTML = countries[1];
-    //     option3.innerHTML = countries[2];
-    // };
+        index++;
+        if(countryIndex >= countries.length){
+            countryIndex = 0;
+        }
+        console.log('hehehe',countries[0]);
+          option1.innerHTML = countries[0];
+         option2.innerHTML = countries[1];
+        option3.innerHTML = countries[2];
+    };
+
+   function loadOptionsB(){
+         option1.innerHTML = countries[3];
+        option2.innerHTML = countries[4];
+        option3.innerHTML = countries[5];
+        nextFlag();
+    }
+
+    function loadOptionsC(){
+    option1.innerHTML = countries[6];
+       option2.innerHTML = countries[7];
+       option3.innerHTML = countries[8];
+       nextFlag();
+   }
+
+    
+   
+
+// start.addEventListner(start)
+
+start.addEventListener('click',function(){
+   
+    console.log('hello',countries[0]);
+   option1.innerHTML = countries[0];
+   option2.innerHTML = countries[1];
+   option3.innerHTML = countries[2];
+
+})
+
+function checkOpt2(){
+    option2.addEventListener('click',function(){
+        if(option2.innerHTML !== answer1){
+            console.log('wrong');
+        } 
+      }) 
+}
+
+function checkOpt3(){
+    option3.addEventListener('click',function(){
+        if(option3.innerHTML !== answer1){
+            console.log('wrong');
+        } 
+      }) 
+}
+   
+   
+
+
+option1.addEventListener('click',function(){
+    if(option1.innerHTML === answer1){
+        console.log('correct'); 
+        console.log('wrong')
+        loadOptionsB()
+        console.log('option B loaded');
+
+        
+     
+    }else{
+        console.log('correct')
+
+        console.log('wrong');
+        checkOpt2();
+
+    }
+})
+
+checkOpt2();
+checkOpt3();
+
+option2.addEventListener('click',function(){
+       if(option2.innerHTML == answer2){
+            console.log('correct');
+            loadOptionsC()
+            console.log('option C loaded');
+        }
+})
+
+//checkOpt2();
+checkOpt3();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+      //next.addEventListener('click',nextCountry);
 
     // next.addEventListener("click", function(){
     //     slideIndex++;
@@ -78,15 +178,4 @@ const countries =  [
 
     
 
-// click function on start     
-
-// start.addEventListner(start)
-
-start.addEventListener('click',function(){
-    // console.log('hello',options[option1]);
-    console.log('hello',countries[0]);
-   option1.innerHTML = countries[0];
-   option2.innerHTML = countries[1];
-   option3.innerHTML = countries[2];
-
-})
+// click function on start    
